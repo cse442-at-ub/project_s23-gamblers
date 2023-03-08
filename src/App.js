c5dba717105091cf7d3e9b1daff11b35a6dcc141
jwu_DESKTOP <wujiawei768@gmail.com>
2023-03-07 23:52:00 -0500

    create a static frontend page for item information #19

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import ItemInfo from "./component/ItemInfo";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/iteminfo">iteminfo</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route index path="/iteminfo" element={<ItemInfo/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
