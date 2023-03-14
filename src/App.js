2d73de94e02173c6215c2e59cd169810653fc464
Embers9527 <89382404+Embers9527@users.noreply.github.com>
2023-03-14 02:15:14 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import ItemInfo from "./components/ItemInfo";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Login from './components/Login'
-import Register from './components/Register'
-import Home from './components/Home'
-import Forgot from './components/Forgot'
-import Verify from './components/Verify'
-import { useState } from 'react'
-import UserAgreement from './components/UserAgreement'
+import './App.css';
+import Services from "./components/Services/Services";
 
 function App() {
-  const [login, setLogin] = useState(false)
-
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route index path="/iteminfo" element={<ItemInfo />} />
-          <Route path='/Login' element={<Login />} />
-          <Route path='/Register' element={<Register />} />
-          <Route path='/Forgot' element={<Forgot />} />
-          <Route path='/UserAgreement' element={<UserAgreement />} />
-          <Route path='/Verify' element={<Verify />} />
-        </Routes>
-      </BrowserRouter>
-      <div>
-        {login && <Login setLogin={setLogin} />}
-      </div>
-
-
-
-
+    <div className="holder">
+      <Services />
     </div>
-  )
+  );
 }
 
 export default App;
