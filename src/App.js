b035fe1108677339672f44db4cfca115d87fe000
Hongjie Lin <hlin57@buffalo.edu>
2023-03-14 19:45:44 -0400

    CreatingSetting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Setting from './components/Setting'
+import Home from './components/Home'
+
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/setting' element={<Setting />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   )
 }
--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,6 +1,7 @@
 import './Setting.css'
 import PopUp from './LogOut'
 import {useState } from 'react'
+import img1 from '../image/icon/unnamed.jpg'
 function Setting(){
     const [buttonPopup, setButtonPopup] = useState(false);
     return(
@@ -15,15 +16,26 @@ function Setting(){
                     <span className='LogOutFont'>Log Out</span>
                 </button>
                     <div className='Icon'>
+                        <div className='circle'>
+                            <img src={img1} alt='icon' />
+                        </div>
+                        <div className='Username'>
+                            <span className='UsernameFont'>Username</span>
+                        </div>
                     
                     </div>
-                <div className='Iconw'></div>
+                <div className='Iconw'>
+                    <button type='button' className='Edituserprofile'>
+                        <span className='EdituserprofileFont'>Edit User Profile</span>
+                    </button>
+                </div>
                 <button className='Myaccount'>
                     <span className='MyaccountFont'>My account</span>
                 </button>
                 <button className='Profile'>
                     <span className='ProfileFont'>Profile</span>
                 </button>
+
                 <PopUp trigger={buttonPopup} setTrigger={setButtonPopup}>
                 </PopUp>
             </div>
