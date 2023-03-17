f1b52509767347b41a79d45a47cb15ee499deb5a
jwu_DESKTOP <wujiawei768@gmail.com>
2023-03-16 23:39:21 -0400

    check if the username is repeat #27

--- api/register/index.php
+++ api/register/index.php
@@ -5,7 +5,6 @@ $objDb = new DbConnect;
 $conn = $objDb->connect();
 $method = $_SERVER['REQUEST_METHOD'];
 $entityBody = file_get_contents('php://input');
-echo $entityBody;
 switch($method){
     case "POST":
         if(empty($_POST)){
@@ -14,10 +13,17 @@ switch($method){
                 $dump['password'] == "" || $dump['phone'] == ""){
                 die("one of you information is empty");
             }
+            $sql = "SELECT * FROM users WHERE username='".$dump['username']."'";
+            $result = $conn->query($sql);
+            $rows = $result->num_rows;
+            if($rows!=0){
+                die("username already exise");
+            }
             $sql = 'INSERT INTO users (email, username, hash ,phoneNumber)
                     VALUES (?,?,?,?)';
             $process = $conn->prepare($sql);
-            $process->bind_param("ssss",$dump['email'],$dump['username'],$dump['password'],$dump['phone']);
+            $pwd_hash = password_hash($dump['password'], PASSWORD_DEFAULT);
+            $process->bind_param("ssss",$dump['email'],$dump['username'],$pwd_hash,$dump['phone']);
             $process->execute();
             echo "New user created";
             $process->close();
