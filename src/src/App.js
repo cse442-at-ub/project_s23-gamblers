bd4da420f04ccaffa74cccfc9296b703f5745837
Embers9527 <89382404+Embers9527@users.noreply.github.com>
2023-03-17 15:37:19 -0400

    Add files via upload

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Services from "./components/Services/Services";
+
+function App() {
+  return (
+    <div className="holder">
+      <Services />
+    </div>
+  );
+}
+
+export default App;
