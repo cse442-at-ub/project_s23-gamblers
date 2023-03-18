7937168c2267704f6e5f2bbf5c5dc9948cb99a85
jwu_DESKTOP <wujiawei768@gmail.com>
2023-03-18 01:17:50 -0400

    module Header.js from ItemInfo.js #19

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import './Header.css'
+import {Col,Navbar,Container,Row} from 'react-bootstrap/';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchBar from './SearchBar';
+import UserImage from './UserImage';
+import brand_image from '../assets/images/exchange.png'
+function Header() {
+    return (
+            <Navbar   className="header" variant="light"  expand="lg">
+            <Container  fluid className="header">
+                <Col md={2}>
+                    <Container variant="dark" className='mt-4 mb-3'>
+                        <Navbar.Brand href="/"><img
+                            alt=""
+                            src={brand_image}
+                            width="60"
+                            height="60"
+                            className="d-inline-block align-top"
+                            />
+                        </Navbar.Brand> 
+                    </Container>
+                </Col>
+                <Col >
+                    <Container fluid className="header d-flex flex-row-reverse mt-4 mb-3">
+                        <a href='/Setting'>
+                            <UserImage></UserImage> 
+                        </a>  
+                        <SearchBar></SearchBar>
+                    </Container>
+                </Col>
+                
+                
+            </Container>
+            </Navbar>
+    )
+}
+export default Header
