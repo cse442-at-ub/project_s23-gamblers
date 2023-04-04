7bd54dde3f4c73eed22a1ab34cdcd9a6b43bbd89
jwu <wujiawei768@gmail.com>
2023-04-04 19:21:44 -0400

    fix CORS error by adding some header in db_connect.php and set cookie when login #34

--- api/database/db_connect.php
+++ api/database/db_connect.php
@@ -1,7 +1,9 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Allow-Methods: *");
+    header("Access-Control-Allow-Methods: POST, PUT, GET");
+    header("Access-Control-Allow-Origin: http://localhost:3000");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization,access-control-allow-origin");
+    
     class DbConnect {
         ########## server #############
         // private $hn = 'oceanus.cse.buffalo.edu:3306';
