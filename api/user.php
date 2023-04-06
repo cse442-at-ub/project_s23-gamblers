ae566c76e4f912c5840f53849af2495dd6ad27b9
jwu <wujiawei768@gmail.com>
2023-04-06 01:49:37 -0400

    add user class post_item function to save item in database #44

--- api/user.php
+++ api/user.php
@@ -20,12 +20,38 @@ class User{
         if(!$this->is_vaild){
             return;
         }
-        
+        $_info = $this->information;
+        $info = ['username' =>$_info['username'],'eamil'=>$_info['email'],"phone_number"=>$_info["phone_number"]]; 
+        echo json_encode($info);        
     }
-    public function postitem(){
+    public function post_item($json){
         if(!$this->is_vaild){
             return;
         }
+        $tb = 'items';
+        $col = '(
+                `user_id`, 
+                `date_posted`, 
+                `item_state`,
+                `last_modify`,
+                `item_name`,
+                `item_image_dir`,
+                `item_description`,
+                `item_price`,
+                `item_contact`)';
+        // dummy 
+        $filename = '/path/to/image.jpg';
+        $path_parts = pathinfo( $filename );
+        $image_name = sql_dots('/uploads/items/'.randomStr(20).'.'.$path_parts['extension']);
+        $item_description = sql_dots('This is an example item description.');
+        $item_price = sql_dots('99');
+        $seller = sql_dots('seller@example.com');
+        $date_posted = sql_dots(date("Y-m-d H:i:s"));
+        $item_name = sql_dots("example i");
+        $poster_id = sql_dots('1');
+        $item_state = sql_dots('active');
+        $val = "($poster_id, $date_posted, $item_state , $date_posted, $item_name, $image_name, $item_description, $item_price, $seller)";
+        insert_tb_cols_values($tb,$col,$val);
     }
     public function change_profile($json){
         if(!$this->is_vaild){
