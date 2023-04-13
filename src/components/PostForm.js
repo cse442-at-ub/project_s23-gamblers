492dd6c11d4cc6d2bcbc4f7c5699a5fb07f39548
Hongjie Lin <hlin57@buffalo.edu>
2023-04-13 18:58:16 -0400

    Implement the comment functionality and improved some minor display issue for PostForm #50

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -2,40 +2,42 @@ import React,{useState} from "react";
 import axios from "axios";
 import "./PostForm.css"
 import toast, { Toaster } from "react-hot-toast";
+import { Link, useNavigate } from 'react-router-dom'
 function PostForm(){
     const url ="https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442m/api/post_item.php"
+    const navigate = useNavigate()
     const [postimage,setPostImage] = useState('')
-    const [data, setData]= useState({
-        item_name:"",
-        description:"",
-        price:"",
-        contact:"",
-    })
-    const [test, setTest] = useState()
+    const [data, setData]= useState([])
     function submit(e){
         e.preventDefault();
         const fd = new FormData()
         fd.append('images', postimage)
         fd.append('item_info',JSON.stringify(data))
         if (data.item_name === "") {
-            window.alert('Please enter item name')
+            toast.error('Please enter item name')
         } else if (data.description===''){
-            window.alert('Please enter item description')
+            toast.error('Please enter item description')
         }else if (data.price === '') {
-            window.alert('Please enter item price')
+            toast.error('Please enter item price')
         }
         else if (data.contact === '') {
-            window.alert('Please enter contact information')
+            toast.error('Please enter contact information')
         }
         else if (postimage === '') {
-            window.alert('Please upload a file')
-        }
+            toast.error('Please upload a file')
+        }else{
         console.log(fd)
         axios.post(url,fd,{withCredentials:true})
         .then(res=>{
-            toast.success('Successfully toasted!')
-            console.log(res.data)
+            if(res.status===200){
+                toast.success('Successfully upload!')
+                setTimeout(function(){
+                    navigate(`/`);
+                 }, 1800);
+                console.log(res.data)
+            }
         })
+    }
     }
 
     function handle(e){
@@ -44,17 +46,15 @@ function PostForm(){
         console.log(e)
         setData(newdata)
         console.log(newdata)
-        console.log(test)
     }
     function handlepostimage(e){
         
-        setTest(e.target.files[0]);
         setPostImage(e.target.files[0])
     }
 
     return (
         <div className="post_main">
-            <div className="sub_main">
+            <div>
                 <Toaster
                 position="top-center"
                 reverseOrder={false}
@@ -65,19 +65,19 @@ function PostForm(){
                 </div>
                 <div className="form-group">
                 <label >Item Name</label>
-                <input className="form-controll" onChange={(e)=>handle(e)} id="item_name" value={data.item_name} placeholder="name" type="text"></input>
+                <input className="form-controll" onChange={(e)=>handle(e)} id="item_name"  placeholder="name" type="text"></input>
                 </div>
                 <div className="form-group">
                 <label>Item Description</label>
-                <input className="form-controll" onChange={(e)=>handle(e)} id="description" value={data.description} placeholder="description" type="text"></input>
+                <input className="form-controll" onChange={(e)=>handle(e)} id="description"  placeholder="description" type="text"></input>
                 </div>
                 <div className="form-group">
                 <label>Item Price</label>
-                <input className="form-controll" onChange={(e)=>handle(e)} id="price" value={data.price} placeholder="price" type="number"></input>
+                <input className="form-controll" onChange={(e)=>handle(e)} id="price" placeholder="price" type="number"></input>
                 </div>
                 <div className="form-group">
                 <label>Contact</label>
-                <input className="form-controll" onChange={(e)=>handle(e)} id="contact"  value={data.contact} placeholder="contact" type="text"></input>
+                <input className="form-controll" onChange={(e)=>handle(e)} id="contact"   placeholder="contact" type="text"></input>
                 </div>
                 <div className="form-group">
                 <label>Image
