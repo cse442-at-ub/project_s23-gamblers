499b5866d28e19570a5905fdc90330608bafd06d
wey <wujiawei768@gmail.com>
2023-04-14 21:01:21 -0400

    add a line to show view count in main page #47 #48

--- src/components/Home.js
+++ src/components/Home.js
@@ -45,6 +45,7 @@ function Home(){
                                         <h4 className={`${styles.text__upper} ${styles.text}`}>{service.item_name}</h4>
                                         <p className={`${styles.para__text} ${styles.text__grey}`}>{service.item_description}</p>
                                         <p className={`${styles.para__text} ${styles.text__grey}`}>${service.item_price} </p>
+                                        <p className={`${styles.para_text_smaller} ${styles.text__grey}`}>View: {service.view_count}</p>
                                         <a href={`/iteminfo?var=${service.item_id}`} className={`${styles.btn}  ${styles.btn__blue}`}>more info</a>
                                     </div>
                                 )
