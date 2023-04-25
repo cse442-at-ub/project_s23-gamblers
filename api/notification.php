6165e877f5fb30e5e4ed46e2ddeeceb2460605d7
wey <wujiawei768@gmail.com>
2023-04-25 16:15:46 -0400

    add Notification page and init functionality done that user can see the notification for deleted liked item and can dismiss it #73

--- api/notification.php
+++ api/notification.php
@@ -1,31 +1,7 @@
 <?php
 include_once 'user.php';
 $method = $_SERVER['REQUEST_METHOD'];
-$entityBody = file_get_contents('php://input');
-if ($method == 'POST'){
-    $dump = json_decode($entityBody,true);
-    $item_id = $dump['item_id'];
-    $item = get_tb_col_value("items","item_id",$item_id);
-    if(!$item ){
-        header("HTTP/1.0 404 Not Found");
-        die();
-    }
-    if(isset($_COOKIE['uid'])){
-        $uid = $_COOKIE['uid'];
-        $infor = get_tb_col_value("cookies","uid",$uid);
-        if($infor){
-            $a = new User($infor['id']);
-            $likes = $a->likes($item['item_id']);
-            echo "success";
-        }
-
-    }
-    else{
-        header('HTTP/1.0 401 Unauthorized');
-        die();
-    }
-    }
-else if ($method == 'GET'){
+if ($method == 'GET'){
     if(isset($_COOKIE['uid'])){
         $uid = $_COOKIE['uid'];
         $infor = get_tb_col_value("cookies","uid",$uid);
@@ -39,7 +15,7 @@ else if ($method == 'GET'){
             $likes = trim($likes,",");
             $sql = "SELECT * 
                     FROM items 
-                    WHERE item_state = 'active' AND
+                    WHERE item_state = 'deleted' AND
                     item_id IN ($likes)";
             $result = get($sql,array(),true);
             echo json_encode($result);
