5c65cb92381175ff8c3debf13661d12782980e30
Tianming Lu <Xiou1812656161@gmail.com>
2023-04-28 16:12:52 -0400

    Sprint4#70-filter done(for $50 dallor example)

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ function Home(){
 
     const [itemData, setItemData] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [filter, setFilter] = useState(0)
 
     function getItem(){
         axios.get(process.env.REACT_APP_BASENAME+'api/all_item').then(function(response){
@@ -26,36 +27,78 @@ function Home(){
         return<p>Loading</p>
     }
     
-    console.log(itemData)
-    return(
-        <div>
-            <Header setItemData={setItemData}></Header>
-            <div className={`${styles.section__padding} ${styles.bg__whitesmoke}`} >
-                <div className={styles.container}>
-                    <div className={`${styles.services__content} ${styles.grid}`}>
-                        {
-                            itemData.map((service, index) => {
-                                return (
-                                    <div className={`${styles.services__contentitem} ${styles.text__center}`} key={index}>
-                                        <div><img
-                                            alt=""
-                                            src={process.env.REACT_APP_BASENAME+`uploads/${itemData[index].item_image_dir}`}
-                                            className='item-image'
-                                        /></div>
-                                        
-                                        <h4 className={`${styles.section__title} ${styles.text}`}>{service.item_name}</h4>
-                                        <p className={`${styles.para__text} ${styles.text__grey}`}>{service.item_description}</p>
-                                        <p className={`${styles.para__text} ${styles.text__grey}`}>${service.item_price} </p>
-                                        <p className={`${styles.para_text_smaller} ${styles.text__grey}`}>View: {service.view_count}</p>
-                                        <Link to={`/iteminfo?var=${service.item_id}`} className={`${styles.btn}  ${styles.btn__blue}`}>more info</Link>
-                                    </div>
-                                )
-                            })
-                        }
+    if(filter===0){
+        return(
+            <div>
+                <Header setItemData={setItemData}></Header>
+                <button className={`${styles.btn}  ${styles.btn__blue}`} onClick={()=>setFilter(1)}>$50 OR LOWER</button>
+                <button className={`${styles.btn}  ${styles.btn__blue}`} onClick={()=>setFilter(0)}>SHOWALL</button>
+                <div className={`${styles.section__padding} ${styles.bg__whitesmoke}`} >
+                    <div className={styles.container}>
+                        <div className={`${styles.services__content} ${styles.grid}`}>
+                            {
+                                itemData.map((service, index) => {
+                                    return (
+                                        <div className={`${styles.services__contentitem} ${styles.text__center}`} key={index}>
+                                            <div><img
+                                                alt=""
+                                                src={process.env.REACT_APP_BASENAME+`uploads/${itemData[index].item_image_dir}`}
+                                                className='item-image'
+                                            /></div>
+                                            
+                                            <h4 className={`${styles.section__title} ${styles.text}`}>{service.item_name}</h4>
+                                            <p className={`${styles.para__text} ${styles.text__grey}`}>{service.item_description}</p>
+                                            <p className={`${styles.para__text} ${styles.text__grey}`}>${service.item_price} </p>
+                                            <p className={`${styles.para_text_smaller} ${styles.text__grey}`}>View: {service.view_count}</p>
+                                            <Link to={`/iteminfo?var=${service.item_id}`} className={`${styles.btn}  ${styles.btn__blue}`}>more info</Link>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }else if(filter===1){
+        return(
+            <div>
+                <Header setItemData={setItemData}></Header>
+                <button className={`${styles.btn}  ${styles.btn__blue}`} onClick={()=>setFilter(1)}>$50 OR LOWER</button>
+                <button className={`${styles.btn}  ${styles.btn__blue}`} onClick={()=>setFilter(0)}>SHOWALL</button>
+                <div className={`${styles.section__padding} ${styles.bg__whitesmoke}`} >
+                    <div className={styles.container}>
+                        <div className={`${styles.services__content} ${styles.grid}`}>
+                            {
+                                itemData.map((service, index) => {
+                                    if(service.item_price<=50){
+                                        return (
+                                            <div className={`${styles.services__contentitem} ${styles.text__center}`} key={index}>
+                                                <div><img
+                                                    alt=""
+                                                    src={process.env.REACT_APP_BASENAME+`uploads/${itemData[index].item_image_dir}`}
+                                                    className='item-image'
+                                                /></div>
+                                                
+                                                <h4 className={`${styles.section__title} ${styles.text}`}>{service.item_name}</h4>
+                                                <p className={`${styles.para__text} ${styles.text__grey}`}>{service.item_description}</p>
+                                                <p className={`${styles.para__text} ${styles.text__grey}`}>${service.item_price} </p>
+                                                <p className={`${styles.para_text_smaller} ${styles.text__grey}`}>View: {service.view_count}</p>
+                                                <Link to={`/iteminfo?var=${service.item_id}`} className={`${styles.btn}  ${styles.btn__blue}`}>more info</Link>
+                                            </div>
+                                        )
+                                    }
+                                    
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+
+    
 }
 export default Home
